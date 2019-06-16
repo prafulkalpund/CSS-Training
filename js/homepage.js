eceef6dad309713cea78c8c9e8f1443387a17415
prafulkalpund <37687876+prafulkalpund@users.noreply.github.com>
2019-06-16 23:08:39 +0530

    New demo site

---
+++ js/homepage.js
@@ -0,0 +1,62 @@
+body{
+    margin: 0;
+}
+
+ul{
+  background: #cce5ff;
+  padding: 0px;
+  margin: 0px;
+}
+
+ul li{
+  padding: 15px 30px;
+  margin: 0px;
+  display: inline-block;
+}
+
+li a{
+  text-decoration: none;
+} 
+
+a{
+  color: black;
+}
+
+.active,
+li:hover{
+  background: #3399ff;
+  
+}
+
+.active a{
+  color: white;
+}
+.sticky{
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.content h3{
+  text-align: center;
+  font-size: 40px;
+  color: cadetblue;
+}
+
+p {
+  color: darkolivegreen;
+  padding: 0 30px;
+}
+
+.content{
+  background-color: honeydew;
+}
+
+img{
+  /* width:   10%; */
+}
+
+.my-header{
+  display: flex;
+  justify-content: space-between; 
+}
