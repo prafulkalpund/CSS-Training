fe68f41488c3c9645ba191c0ce1ab91b8cab14a6
praful.kalpund <praful.kalpund@wisdmlabs.com>
2019-06-26 20:43:54 +0530

    smart slider

---
+++ smart/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Slider 3</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<img src="img/logo.png" alt="logo">
+		<nav>
+			<a href="" title="">Help</a>
+			<a href="" title="">Features</a>
+			<a href="" title="">Blog</a>
+			<a href="" title="">Try</a>
+			<a href="" title="">Free</a>
+			<a href="" title="">Account</a>
+			<a class="btn btn-active example" href="" title="">Examples</a>
+			<a class="btn btn-selected " href="" title="">Buy now</a>
+		</nav>
+	</header>
+	<main>
+		<section class="banner">
+			<div class="banner-wrapper">
+				<div class="banner-text">
+					<h1>
+						Smart <span>Slider 3</span>
+					</h1>
+					<h3>
+						Make beautiful and powerful sliders, carousels and blocks. Designing sliders is finally fun!
+					</h3>
+					<a class="btn btn-banner" href="" title="">Get started</a>
+				</div>
+				<div class="banner-img"></div>
+
+			</div>
+		</section>
+	</main>
+</body>
+</html>
